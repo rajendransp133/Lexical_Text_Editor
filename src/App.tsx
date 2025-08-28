eee86761881e382bdcedcc53aba5ac7a8c75c537
Rajendran Sp <85285414+rajendransp133@users.noreply.github.com>
2025-08-28 15:38:02 +0530

    basic rich text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,159 @@
-// import { Editor } from "./EditorArticle";
-// import { Video1 } from "./Video1";
-// import { Video1Continue } from "./Video1Continue";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import {
+  $isTextNode,
+  type DOMConversionMap,
+  type DOMExportOutput,
+  type DOMExportOutputMap,
+  isHTMLElement,
+  type Klass,
+  type LexicalEditor,
+  type LexicalNode,
+  ParagraphNode,
+  TextNode,
+} from "lexical";
 
-import { Editor } from "./Editor/Editor.tsx";
+import ExampleTheme from "./ExampleTheme";
+import ToolbarPlugin from "./Plugins/ToolbarPlugin";
+import { parseAllowedColor, parseAllowedFontSize } from "./styleConfig";
 
-// function App() {
-//   return (
-//     <div className="p-4 flex flex-col gap-4">
-//       <h1 className="text-3xl font-bold underline">Hello World</h1>
-//       <Editor />
-//       <Video1 />
-//       <Video1Continue />
-//     </div>
-//   );
-// }
+const placeholder = "Enter some rich text...";
+
+const removeStylesExportDOM = (
+  editor: LexicalEditor,
+  target: LexicalNode
+): DOMExportOutput => {
+  const output = target.exportDOM(editor);
+  if (output && isHTMLElement(output.element)) {
+    // Remove all inline styles and classes if the element is an HTMLElement
+    // Children are checked as well since TextNode can be nested
+    // in i, b, and strong tags.
+    for (const el of [
+      output.element,
+      ...output.element.querySelectorAll("[style],[class]"),
+    ]) {
+      el.removeAttribute("class");
+      el.removeAttribute("style");
+    }
+  }
+  return output;
+};
+
+const exportMap: DOMExportOutputMap = new Map<
+  Klass<LexicalNode>,
+  (editor: LexicalEditor, target: LexicalNode) => DOMExportOutput
+>([
+  [ParagraphNode, removeStylesExportDOM],
+  [TextNode, removeStylesExportDOM],
+]);
+
+const getExtraStyles = (element: HTMLElement): string => {
+  // Parse styles from pasted input, but only if they match exactly the
+  // sort of styles that would be produced by exportDOM
+  let extraStyles = "";
+  const fontSize = parseAllowedFontSize(element.style.fontSize);
+  const backgroundColor = parseAllowedColor(element.style.backgroundColor);
+  const color = parseAllowedColor(element.style.color);
+  if (fontSize !== "" && fontSize !== "15px") {
+    extraStyles += `font-size: ${fontSize};`;
+  }
+  if (backgroundColor !== "" && backgroundColor !== "rgb(255, 255, 255)") {
+    extraStyles += `background-color: ${backgroundColor};`;
+  }
+  if (color !== "" && color !== "rgb(0, 0, 0)") {
+    extraStyles += `color: ${color};`;
+  }
+  return extraStyles;
+};
+
+const constructImportMap = (): DOMConversionMap => {
+  const importMap: DOMConversionMap = {};
+
+  // Wrap all TextNode importers with a function that also imports
+  // the custom styles implemented by the playground
+  for (const [tag, fn] of Object.entries(TextNode.importDOM() || {})) {
+    importMap[tag] = (importNode) => {
+      const importer = fn(importNode);
+      if (!importer) {
+        return null;
+      }
+      return {
+        ...importer,
+        conversion: (element) => {
+          const output = importer.conversion(element);
+          if (
+            output === null ||
+            output.forChild === undefined ||
+            output.after !== undefined ||
+            output.node !== null
+          ) {
+            return output;
+          }
+          const extraStyles = getExtraStyles(element);
+          if (extraStyles) {
+            const { forChild } = output;
+            return {
+              ...output,
+              forChild: (child, parent) => {
+                const textNode = forChild(child, parent);
+                if ($isTextNode(textNode)) {
+                  textNode.setStyle(textNode.getStyle() + extraStyles);
+                }
+                return textNode;
+              },
+            };
+          }
+          return output;
+        },
+      };
+    };
+  }
+
+  return importMap;
+};
+
+const editorConfig = {
+  html: {
+    export: exportMap,
+    import: constructImportMap(),
+  },
+  namespace: "React.js Demo",
+  nodes: [ParagraphNode, TextNode],
+  onError(error: Error) {
+    throw error;
+  },
+  theme: ExampleTheme,
+};
 
 function App() {
   return (
     <div className="max-w-4xl mx-auto px-8 my-16">
-      <h1 className="text-3xl font-serif">Lexical React Demo</h1>
-      <Editor />
+      <h1>React.js Rich Text Lexical Example</h1>
+      <LexicalComposer initialConfig={editorConfig}>
+        <div className="editor-container">
+          <ToolbarPlugin />
+          <div className="editor-inner">
+            <RichTextPlugin
+              contentEditable={
+                <ContentEditable
+                  className="editor-input"
+                  aria-placeholder={placeholder}
+                  placeholder={
+                    <div className="editor-placeholder">{placeholder}</div>
+                  }
+                />
+              }
+              ErrorBoundary={LexicalErrorBoundary}
+            />
+            <HistoryPlugin />
+            <AutoFocusPlugin />
+          </div>
+        </div>
+      </LexicalComposer>
     </div>
   );
 }
